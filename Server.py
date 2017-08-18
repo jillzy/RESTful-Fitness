87be4a67d3cb1092d9e3d57b4d2a93c23fe56540
jillzy <jillzyeung@gmail.com>
2017-08-18 16:36:33 -0700

    firebase email authentication

--- Server.py
+++ Server.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
+#@app.route('/')
+#def hello_world():
+#    return 'hello, world!
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def webprint():
+    return render_template('FitnessApp.html')
+
 
 if __name__ == '__main__':
     app.run()
