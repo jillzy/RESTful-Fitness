468e6af6701c20f66635114d2e5ee4ed58ccf857
jillzy <jillzyeung@gmail.com>
2017-08-20 03:59:18 -0700

    send and handle (placeholder) post requests for lift button

--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -8,11 +9,21 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/action_page', methods=['POST'])
+def action():
+    #take string and parse/loads into json object
+    data = json.loads(request.get_data())
+    print(data)
+    print(type(data))
+    benchVariations = ['Bench, Incline Bench, Dumbbell Press']
+    #turns json back into str
+    return json.dumps(data)
+
+
 @app.route('/')
 def index():
     lifts = ['Bench', 'Deadlift', 'Squat'];
-    benchVariations = ['Bench, Incline Bench, Dumbbell Press']
-    return render_template('layout.html', lifts = lifts, benchV = benchVariations)
+    return render_template('layout.html', lifts=lifts)
 
 
 
