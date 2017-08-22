4e3e288e4de459470b824dc12deb9fe8273499ea
jillzy <jillzyeung@gmail.com>
2017-08-21 19:08:39 -0700

    separate scripts to external file

---
+++ static/scripts.js
@@ -0,0 +1,55 @@
+var lastPressed = "";
+
+function toggle(item){
+	var clicked = document.getElementById(item+'Expansion');
+	var lastClicked = "";
+	if (lastPressed != "") {
+		lastClicked = document.getElementById(lastPressed+'Expansion');
+		console.log(lastPressed+'Expansion');
+	}
+	var content = document.createTextNode(item + "variations");
+	
+	//our first click
+	if (lastClicked == "") { 
+		clicked.appendChild(content);
+		clicked.style.display = "block"
+
+	//nothing is currently opened
+	} else if (lastClicked.style.display == "none") {
+		clicked.appendChild(content);
+		clicked.style.display = "block";
+ 
+	//something is currently opened
+	} else if (lastClicked.style.display == "block") {
+		//we reclicked our last click
+		if (clicked == lastClicked) {
+			lastClicked.style.display = "none";
+			lastClicked.innerHTML = "";
+ 
+		//and we clicked something else
+		} else if (clicked != lastClicked) {
+			lastClicked.style.display = "none";
+			lastClicked.innerHTML = "";
+			clicked.appendChild(content);
+			clicked.style.display = "block";
+ 
+		}
+	}
+	lastPressed = item;
+}
+
+function createExpansion(name) {
+	var div = document.createElement('div');
+	div.id = name+"Expansion";
+	div.style.border = '1px solid black';
+	div.style.position= 'absolute';
+	div.onmouseup = dynamicEvent; //todo: eentuallyt his is the elements of the expansion
+	div.style.display = 'none';
+	document.body.appendChild(div);
+	return div;
+}
+
+function dynamicEvent() {
+	alert("works");
+}
+
