f324ec50e03457ea64d609397766c4970d820b5c
jillzy <jillzyeung@gmail.com>
2017-08-23 10:58:34 -0700

    create links for each variation

--- static/scripts.js
+++ static/scripts.js
@@ -1,37 +1,50 @@
-var lastPressed = "";
 var secondaryLinkData = "";
 var data;
 
 function toggleOn(item){
 	
-	console.log("secondaryLinkData is: " + secondaryLinkData);
+	var mainButton = document.getElementById(item + 'Link');
 	
 	if (secondaryLinkData != "") {
 		document.getElementById(item + 'Link')
-			.appendChild(createExpansion(item));
+			.appendChild(createExpansionContainer(item));
+		
+		var expansionContainer = document.getElementById(item+'ExpansionContainer');
+		var dict = JSON.parse(secondaryLinkData);
+		
+		for (var key in dict){
+			for (var i = 0; i < dict[key].length; i++) {
+				var secondaryLinkDiv = createSecondaryLink(i);
+				var content = document.createTextNode(dict[key][i]);
+				secondaryLinkDiv.appendChild(content);
+				console.log("append '" + content.textContent + "' to "
+					+ secondaryLinkDiv.id);
+
+				expansionContainer.appendChild(secondaryLinkDiv);
+				console.log("append " + secondaryLinkDiv.id + " to "
+					+ expansionContainer.id);
+			}
+
+		}
+		
 	}
 	
-	var mainButton = document.getElementById(item + 'Link');
-	var clicked = document.getElementById(item+'Expansion');
-	var content = document.createTextNode(secondaryLinkData);
-	
 	mainButton.style.fontWeight = "bold";
-	clicked.innerHTML = "";
-	clicked.appendChild(content);
-	clicked.style.display = "inline-block";
-	
-	lastPressed = item;
+	expansionContainer.style.display = "inline-block";
+
 }
 
 
 function toggleOff(item){
 
 	var mainButton = document.getElementById(item + 'Link');
-	var clicked = document.getElementById(item+'Expansion');
-	
+	if (document.getElementById(item+'ExpansionContainer') != null) {
+		var expansionContainer = document.getElementById(item+'ExpansionContainer');
+			expansionContainer.style.display = "none";
+			expansionContainer.innerHTML = "";
+	}
 	mainButton.style.fontWeight = "normal";
-	clicked.style.display = "none";
-	clicked.innerHTML = "";
+
 
 }
 
@@ -49,15 +62,23 @@ function handleData(data) {
 	//var div = document.createElement('div');
 	//append as a child in expansion
 	secondaryLinkData = data;
-	console.log("set secondaryLinkData to: " + secondaryLinkData);
 }
 
-function createExpansion(name) {
+function createExpansionContainer(name) {
 	var div = document.createElement('div');
-	div.id = name+"Expansion";
-	div.className = 'secondaryLinks';
-	div.onmouseup = dynamicEvent; //todo: eentuallyt his is the elements of the expansion
+	div.id = name+"ExpansionContainer";
+	div.className = 'expansionContainer';
+	//div.onmouseup = dynamicEvent; //todo: eentuallyt his is the elements of the expansion
 	document.body.appendChild(div);
+	return div;
+}
+
+function createSecondaryLink(i) {
+	var div = document.createElement('div');
+	div.id = "SecondaryLink"+i;
+	div.className = 'secondaryLinks';
+	div.onmouseup = dynamicEvent;
+	//document.body.appendChild(div);
 	return div;
 }
 
